cc10727ae398da72342b5cb9d87e0f33641a7376
Cody Phillips <codyphillips@ymail.com>
2023-11-21 06:13:07 -0600

    darkmode

--- src/components/ModeToggler.tsx
+++ src/components/ModeToggler.tsx
@@ -1,11 +1,18 @@
-export default Modetoggler() {
-    const darkModeOn = true;
+export default function ModeToggler() {
+    let darkModeOn = true;
     const darkMode = <h1>Dark Mode is On</h1>
     const lightMode = <h1>Light Mode is On</h1>
+
+    function handleClick() {
+      darkModeOn = !darkModeOn;
+    }
 
     return (
       <div>
         {darkModeOn ? darkMode : lightMode}
+        <button onClick = {handleClick}>
+          Click me
+        </button>
       </div>
     )
 }
