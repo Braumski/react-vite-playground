3aeb77104a275a4f1ee8f1385d4995b8f0564656
Cody Phillips <codyphillips@ymail.com>
2023-12-01 07:15:35 -0600

    im dumb 🪦

--- src/components/ReactVideoPlayerLibrary.tsx
+++ src/components/ReactVideoPlayerLibrary.tsx
@@ -4,5 +4,5 @@ export default function ReactVideoPlayerLibrary(){
   /* This component is basically pointless, since I could just put
   the react-player directly in the App Component, but I want a clear File
   structure for now */
-  return <ReactPlayer url='https://www.youtube.com/watch?v=i_0G6WPuss4'
+  return <ReactPlayer url='https://www.youtube.com/watch?v=i_0G6WPuss4'/>
 }
