794f176d5d08b0041485de4e8b145aaad1731a68
Cody Phillips <codyphillips@ymail.com>
2023-12-01 07:21:16 -0600

    editing video controls from library, cool stuff 😎

--- src/components/ReactVideoPlayerLibrary.tsx
+++ src/components/ReactVideoPlayerLibrary.tsx
@@ -3,6 +3,14 @@ import ReactPlayer from 'react-player'
 export default function ReactVideoPlayerLibrary(){
   /* This component is basically pointless, since I could just put
   the react-player directly in the App Component, but I want a clear File
-  structure for now */
-  return <ReactPlayer url='https://www.youtube.com/watch?v=i_0G6WPuss4'/>
+  structure for now (but it does hide the ugly props from the app component :)*/
+  return (
+  <ReactPlayer
+    url='https://www.youtube.com/watch?v=i_0G6WPuss4'
+    controls='true'
+    loop='true'
+    playing='true'
+   />
+  )
+  
 }
