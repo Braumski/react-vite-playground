52c84465249b545803d8d99292c264f7ef824277
Cody Phillips <codyphillips@ymail.com>
2023-12-02 13:27:29 -0600

    almost made the decrement button increment...

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -6,22 +6,18 @@ export default function Counter (){
 
   // The first value in the array destructuring of useState is always the starting value
   // The second value allows you to update your state.
-  function decrementCount(event){
+  function decrementCount(event: React.MouseEvent<HTMLButtonElement>){
     event.ctrlKey ? 
     setCount(prevCount => prevCount + 100)
     :setCount(prevCount=>  prevCount + 1) // Set count is written as a function like so
   }
-  function incrementCount(event){
-    event.ctrlKey ? 
-      setCount(prevCount => prevCount + 100)
-      :setCount(prevCount=>  prevCount + 1)
 
-    // if (event.ctrlKey){
-    //   setCount(prevCount => prevCount + 100) 
-    // } else{
-    // setCount(prevCount => prevCount + 5)
-    // }
+  function incrementCount(event: React.MouseEvent<HTMLButtonElement>){
+    event.ctrlKey ? 
+      setCount(prevCount => prevCount - 100)
+      :setCount(prevCount=>  prevCount - 1)
   }
+
   return (
     <>
     <h1>Hold Ctrl + Click to Count 100</h1>
