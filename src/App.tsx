83c32eb1f96ab2adb946197a2a707d3db2430038
Cody Phillips <codyphillips@ymail.com>
2023-12-02 13:19:52 -0600

    this page is so silly and ugly 😁. I used ternary operators and ctrl + click to have a different effect on my counter. I also made the links have a pretty animation 🤩

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import {Routes, Route, Link} from 'react-router-dom'
 
 // My files
 import ModeToggler from './components/ModeToggler';
-import {Dog, Puppy, Bowl} from './components/PropsPassing'
+import {Dog} from './components/PropsPassing'
 import ReactVideoPlayerLibrary from './components/ReactVideoPlayerLibrary';
 import Counter from './components/Counter'
 import UsingImages from './components/UsingImages'
@@ -14,17 +14,21 @@ function App() {
   return (
     <>
       <ModeToggler />
-      <Dog />
 
-      <Counter />
-      <UsingImages/>
-      <ReactVideoPlayerLibrary/>
+      <nav className="nav">
+        {/* Link Tags replace Anchor or <a href> tags in React */}
+        <Link to="/dog-homepage" className="nav-item">Dog</Link>
+        <Link to="/counter" className="nav-item">Counter</Link>
+        <Link to="/cat" className="nav-item">My Cat</Link>
+        <Link to="/cool-song" className="nav-item">Cool Song</Link>
+      </nav>
 
-      {/* <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/about-me" element={<AboutMe />}/>
-        <Route path="/conditional-render" element={<ConditionalRender/>} />
-      </Routes> */}
+      <Routes>
+        <Route path="/dog-homepage" element={<Dog />}/>
+        <Route path="/counter" element={<Counter />}/>
+        <Route path="/cat" element={<UsingImages/>} />
+        <Route path="/cool-song" element={<ReactVideoPlayerLibrary/>} />
+      </Routes>
 
     </>
   )
