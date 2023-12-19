e4a311b214d6deb6dd79a0425d0aa15539129505
Cody Phillips <codyphillips@ymail.com>
2023-12-19 07:00:35 -0600

    conditional color stylings for submit btn

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,40 @@
+import { useState } from "react"
+
 export default function Form(){
+  const [name, setName] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault() //Stops page refresh
+    setName("") // Clears the input box
+    console.log("Form Submitted!")
+  }
+
+  
+
+
   return(
-    <form>
+    <form onSubmit={handleSubmit}>
       <fieldset id="name-fieldset">
         <div className="field">
           <label>Name:</label>
-          <input type="text" placeholder="Name" name="name"/>
+          <input 
+            type="text" 
+            placeholder="Name" 
+            name="name" 
+            value={name} 
+            onChange={(e=> setName(e.target.value))}
+          />
         </div>
-        <button id="submit" type="submit">Submit</button>
+        <button
+          id="submit"
+          type="submit"
+          disabled={!name}
+          style={{
+            backgroundColor: name ? 'var(--primary-color)': 'rgba(var(--primary-color), 0.9)',
+            color: name ? 'var(--primary-light)' : 'var(--secondary-light)'
+          }} //Does not allow submission if there is no input
+        >Submit
+        </button>
       </fieldset>
     </form>
   )
