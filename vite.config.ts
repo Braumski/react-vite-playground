c5042aa6d1e34131a741140317c7365791fead7f
Cody Phillips <codyphillips@ymail.com>
2023-12-23 12:52:11 -0600

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-vite-playground/'
+  base: '/react-vite-playground/',
+  build: {
+    outDir: 'dist',
+  },
 })
