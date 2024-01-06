8c7ddacfefd7d6f51aefde7df2f0038b0ef3f864
Cody Phillips <codyphillips@ymail.com>
2024-01-06 06:45:50 -0600

    Importing material UI

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,13 @@
+import SearchIcon from "@mui/icons-material/Search";
+
 export default function SearchBar({ placeholder, data }) {
   return (
     <div className="search">
       <div className="search-inputs">
         <input type="text" placeholder="Enter a pokemon name" />
-        <div className="search-icon"></div>
+        <div className="search-icon">
+          <svg data-testid="SearchIcon"></svg>
+        </div>
       </div>
       <div className="data-result"></div>
     </div>
