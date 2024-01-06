23d793ef65ed015c6df5fe2f8e35c63f040f99d3
Cody Phillips <codyphillips@ymail.com>
2024-01-06 07:30:45 -0600

    Material UI crashes my app, might use something else

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,12 @@
 import SearchIcon from "@mui/icons-material/Search";
 
-export default function SearchBar({ placeholder, data }) {
+export default function SearchBar() {
   return (
     <div className="search">
       <div className="search-inputs">
         <input type="text" placeholder="Enter a pokemon name" />
         <div className="search-icon">
-          <svg data-testid="SearchIcon"></svg>
+          <SearchIcon />
         </div>
       </div>
       <div className="data-result"></div>
