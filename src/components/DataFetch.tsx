375cdb805eebaa6fcedf0aca46d1de2335ef0f59
Cody Phillips <codyphillips@ymail.com>
2024-01-10 06:26:25 -0600

    making a search bar that displays pkmn

--- src/components/DataFetch.tsx
+++ src/components/DataFetch.tsx
@@ -16,24 +16,18 @@ interface Pokemon {
 export default function DataFetch() {
   const [pokemon, setPokemon] = useState<Pokemon | null>(null);
   const inputRef = useRef<HTMLInputElement>(null);
-  const [inputValue, setInputValue] = useState();
-  const { theme } = useContext(ThemeContext);
   const pokemonList = [];
 
   fetch(`https://pokeapi.co/api/v2/pokemon/?limit=1025`) // all 1025 pokemon for search results
     .then((response) => response.json())
     .then((data) => {
       const pokemonArray = data.results;
-      pokemonArray.forEach((pokemon, id) => {
-        const name = pokemon.name;
-        const pokemonObject = { id, name };
-        pokemonList.push(pokemonObject);
+      pokemonArray.forEach((pokemon) => {
+        pokemonList.push(pokemon.name);
       });
       console.log(pokemonList); // this list is for the autocomplete search bar
       //  expect an array of objects with 2 properties, id and pokemon
       //  [ {id: 0, name: 'bulbasaur'}, {id: 1, name: 'ivysaur'}, {id: 2, name: 'venusaur'} ...]
-
-      // Might have to get rid of this since the last library didnt work.
     })
     .catch((error) => {
       console.error("Something went wrong with retrieving the pokemon!");
@@ -60,43 +54,9 @@ export default function DataFetch() {
     }
   };
 
-  function handleSubmit(event: React.FormEvent) {
-    event.preventDefault();
-    fetchPokemonData();
-  }
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
-  };
-
   return (
     <>
-      <form className="pkmn-search" onSubmit={handleSubmit}>
-        <label htmlFor="pokemon-search">Search:</label>
-        <input
-          type="search"
-          id={theme}
-          name="pokemon-search"
-          placeholder="Enter a Pokemon name"
-          ref={inputRef}
-          onChange={handleInputChange}
-          value={inputValue}
-        />
-        <button type="submit">Search</button>
-      </form>
-
-      <SearchBar />
-
-      {pokemon ? (
-        <>
-          <h1>Data returned</h1>
-          <h2>
-            {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
-          </h2>
-          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-        </>
-      ) : (
-        <h1>Data Pending...</h1>
-      )}
+      <SearchBar pokemonList={pokemonList} />
     </>
   );
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,15 +1,20 @@
-import SearchIcon from "@mui/icons-material/Search";
+import { FaSearch } from "react-icons/fa";
 
-export default function SearchBar() {
+export default function SearchBar({ pokemonList }) {
+  console.log(pokemonList);
   return (
     <div className="search">
       <div className="search-inputs">
         <input type="text" placeholder="Enter a pokemon name" />
         <div className="search-icon">
-          <SearchIcon />
+          <FaSearch />
         </div>
       </div>
-      <div className="data-result"></div>
+      <div className="data-result">
+        {pokemonList.map((name, key) => {
+          return <a>{name}</a>;
+        })}
+      </div>
     </div>
   );
 }
